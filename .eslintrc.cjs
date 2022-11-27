64c8bc4a3769d1ca63f1de5cb78a28b50eaa817e
vitordevsp <contatodevsp@gmail.com>
2022-11-27 13:23:04 -0300

    chore: adiciona regras no eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript"
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest"
+  },
+  rules: {
+    "quotes": ["warn", "double"],
+    "semi": ["warn", "never"],
+    "eol-last": ["warn", "always"],
+    "no-multiple-empty-lines": ["warn", { "max": 1, "maxEOF": 0 }],
+    "no-console": ["warn", { "allow": ["warn", "error"] }]
   }
 }
