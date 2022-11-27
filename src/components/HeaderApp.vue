5bd1df699031a8b3d5218508cc1ba632fe84fa8b
vitordevsp <contatodevsp@gmail.com>
2022-11-27 14:43:20 -0300

    feat: cria o comp. HeaderApp

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import IconLogoEstar from "@/assets/icons/IconLogoEstar.vue"
+</script>
+
+<template>
+  <header>
+    <IconLogoEstar />
+    <span>(Desafio Boleta)</span>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+}
+
+header svg {
+  width: 200px;
+  height: 60px;
+}
+
+header span {
+  opacity: 0.7;
+  font-size: 12px;
+}
+</style>
