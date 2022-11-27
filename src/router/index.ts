e1bd14d1a4915ef031fb8d6de68e65b1230a2ef9
vitordevsp <contatodevsp@gmail.com>
2022-11-27 13:25:47 -0300

    style(lint): ajusta o estilo do codigo de acordo com as regras do eslint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import WelcomeView from '../views/WelcomeView.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import WelcomeView from "../views/WelcomeView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'welcome',
+      path: "/",
+      name: "welcome",
       component: WelcomeView
     },
     {
-      path: '/boleta',
-      name: 'boleta',
-      component: () => import('../views/BoletaView.vue')
+      path: "/boleta",
+      name: "boleta",
+      component: () => import("../views/BoletaView.vue")
     }
   ]
 })
