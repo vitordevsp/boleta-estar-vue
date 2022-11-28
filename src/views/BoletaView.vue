2017b4511a8e2222eaedc947e8d8ef8ab30f1e9e
vitordevsp <contatodevsp@gmail.com>
2022-11-27 21:34:17 -0300

    feat: view BoletaView

--- src/views/BoletaView.vue
+++ src/views/BoletaView.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
+import HeaderApp from "@/components/HeaderApp.vue"
+import OrderBook from "@/components/OrderBook.vue"
+import BoletaCard from "@/components/BoletaCard.vue"
 </script>
 
 <template>
-  <h1>Pagina da Boleta</h1>
+  <main>
+    <HeaderApp />
+
+    <section id="home-broker">
+      <OrderBook />
+      <BoletaCard />
+    </section>
+  </main>
 </template>
 
 <style scoped>
+main {
+  min-height: 100vh;
+  padding: 32px 60px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+}
 
+#home-broker {
+  display: flex;
+  gap: 24px;
+}
+
+#order-book {
+  flex-grow: 4;
+}
+
+#boleta-card {
+  flex-grow: 1;
+}
 </style>
