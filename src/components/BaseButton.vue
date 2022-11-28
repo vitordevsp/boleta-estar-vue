ee44b0c7a8918bd0a3c69ee9668ad4e0776bb67b
vitordevsp <contatodevsp@gmail.com>
2022-11-27 21:30:39 -0300

    feat: comp. BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface IBaseButton {
+  variant: "primary-color" | "secondary-color"
+  onClick?: () => void
+  disable?: boolean
+}
+
+defineProps<IBaseButton>()
+</script>
+
+<template>
+  <button :class="variant" @click="onClick" :disabled="disable">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  padding: 16px 12px;
+  border-radius: 12px;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+button:disabled {
+  opacity: 0.7;
+  cursor: no-drop;
+}
+
+.primary-color {
+  background-color: var(--color-primary);
+  color: var(--color-text-dark);
+}
+
+.secondary-color {
+  background-color: var(--color-secondary);
+  color: var(--color-text);
+}
+</style>
